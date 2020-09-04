380d4fa91cb152fc2632d20d182bb91c8122f1e7
Bryan Colin <bryancolin35@ymail.com>
2020-09-04 14:27:19 +0700

    Update Eye Assets

--- Assets/Scripts/PacMan/PacManMovement.cs
+++ Assets/Scripts/PacMan/PacManMovement.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class PacManMovement : MonoBehaviour
 {
     public Animator animatorController;
+    private GameObject gameObject;
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
