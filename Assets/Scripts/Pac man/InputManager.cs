b5f1e2f74024a67e977b15915ffebc8936bcf0d6
Bryan Colin <bryancolin35@ymail.com>
2020-09-12 20:32:16 +0700

    Update Pac Man Tweener Done 1
    Twenner, Rotating with Pac Man Animation complete
    Audio Clip is still not working yet

---
+++ Assets/Scripts/Pac man/InputManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class InputManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject item;
+    [SerializeField]
+    private float timeDuration;
+    private Tweener tweener;
+    //public AudioClip audioClip;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tweener = GetComponent<Tweener>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeDuration != 0)
+        {
+            if (item.transform.position.x == -1.5f && item.transform.position.y == 0.0f)
+            {
+                tweener.AddTween(item.transform, item.transform.position, new Vector3(-12.5f, 0.0f, 0.0f), timeDuration);
+                item.GetComponent<Animator>().SetTrigger("Left");
+            }
+            if (item.transform.position.x == -12.5f && item.transform.position.y == 0.0f)
+            {
+                tweener.AddTween(item.transform, item.transform.position, new Vector3(-12.5f, 13.0f, 0.0f), timeDuration);
+                item.GetComponent<Animator>().SetTrigger("Up");
+            }
+            if (item.transform.position.x == -12.5f && item.transform.position.y == 13.0f)
+            {
+                tweener.AddTween(item.transform, item.transform.position, new Vector3(-1.5f, 13.0f, 0.0f), timeDuration);
+                item.GetComponent<Animator>().SetTrigger("Right");
+            }
+            if (item.transform.position.x == -1.5f && item.transform.position.y == 13.0f)
+            {
+                tweener.AddTween(item.transform, item.transform.position, new Vector3(-1.5f, 0.0f, 0.0f), timeDuration);
+                item.GetComponent<Animator>().SetTrigger("Down");
+            }
+                //AudioSource.PlayClipAtPoint(audioClip, item.transform.position);
+        }
+    }
+}
+
---
+++ Assets/Scripts/Pac man/Tweener.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tweener : MonoBehaviour
+{
+    private Tween activeTween;
+
+    public void AddTween(Transform targetObject, Vector3 startPos, Vector3 endPos, float duration)
+    {
+        if (activeTween == null)
+        {
+            activeTween = new Tween(targetObject, startPos, endPos, Time.time, duration);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (activeTween != null)
+        {
+            float distance = Vector3.Distance(activeTween.Target.transform.position, activeTween.EndPos);
+            Debug.Log("Distance : " + distance);
+
+            if (distance > 0.1f)
+            {
+                float timeFraction = (Time.time - activeTween.StartTime) / activeTween.Duration;
+                Debug.Log("Time : " + timeFraction);
+                activeTween.Target.transform.position = Vector3.Lerp(activeTween.StartPos, activeTween.EndPos, timeFraction);
+            }
+
+            if (distance <= 0.1f)
+            {
+                activeTween.Target.position = activeTween.EndPos;
+                activeTween = null;
+            }
+        }
+    }
+
+    public void Linear(Tween activeTween)
+    {
+        float timeFraction = (Time.time - activeTween.StartTime) / activeTween.Duration;
+        //Debug.Log("Time : " + timeFraction);
+        activeTween.Target.transform.position = Vector3.Lerp(activeTween.StartPos, activeTween.EndPos, timeFraction);
+    }
+
+    public void Cubic(Tween activeTween)
+    {
+        float timeFraction = Mathf.Pow((Time.time - activeTween.StartTime) / activeTween.Duration, 3);
+        //Debug.Log("Time : " + timeFraction);
+        activeTween.Target.transform.position = Vector3.Lerp(activeTween.StartPos, activeTween.EndPos, timeFraction);
+    }
+}
