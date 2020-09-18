b4e9a79c104b35311a734355adb46ddbffe64810
Bryan Colin <bryancolin35@ymail.com>
2020-09-18 20:27:03 +0700

    Update Background Intro Music Script on Main Camera

---
+++ Assets/Scripts/AudioScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioScript : MonoBehaviour
+{
+    [SerializeField]
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.enabled = false;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 0;
+        audioSource.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!audioSource.isPlaying)
+        {
+            Time.timeScale = 1;
+        }
+    }
+}
