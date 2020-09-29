fac2692381fcfb6c1a3309f2e59da57c8f52c5a8
Bryan Colin <bryancolin35@ymail.com>
2020-09-29 22:16:50 +0700

    Create PacStudentController
    Tweener and RayCast on Progress

---
+++ Assets/Scripts/Character/Pac man/PacStudentController.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PacStudentController : MonoBehaviour
+{
+    private Vector3 movement;
+    private Tweener tweener;
+
+    private KeyCode lastInput;
+
+    public float walkSpeed = 1.5f;
+    public float timeDuration = 4.5f;
+
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tweener = GetComponent<Tweener>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetMovementInput();
+        CharacterPosition();
+        WalkingAnimation();
+        Tweening();
+    }
+
+    void GetMovementInput()
+    {
+        movement.x = Input.GetAxis("Horizontal");
+        movement.y = Input.GetAxis("Vertical");
+
+        movement = Vector3.ClampMagnitude(movement, 1.0f);
+
+        if (Input.anyKeyDown)
+        {
+            lastInput = Event.current.keyCode;
+            Debug.Log(lastInput);
+        }
+
+    }
+
+    void CharacterPosition()
+    {
+        transform.Translate(movement * walkSpeed * Time.deltaTime, Space.World); 
+    }
+
+    void WalkingAnimation()
+    {
+        if (movement != Vector3.zero)
+        {
+            animator.SetFloat("Horizontal", movement.x);
+            animator.SetFloat("Vertical", movement.y);
+        }
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+    }
+
+    void Tweening()
+    {
+        tweener.AddTween(transform, transform.position, transform.position + movement, timeDuration);
+    }
+
+    bool RayCastCheck()
+    {
+        RaycastHit hitInfo;
+        bool hit = Physics.Raycast(transform.position + new Vector3(0, 1, 0), transform.forward, out hitInfo, 0.5f);
+        if (hit)
+        {
+            Debug.Log("Raycast Hit: " + hitInfo.collider.name);
+            if (hitInfo.collider.CompareTag("Freeze"))
+                return true;
+        }
+        return false;
+    }
+}
---
+++ Assets/Scripts/Character/Pac man/Tween.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tween
+{
+    Transform target;
+    Vector3 startPos, endPos;
+    float startTime, duration;
+
+    public Tween(Transform target, Vector3 startPos, Vector3 endPos, float startTime, float duration)
+    {
+        this.target = target;
+        this.startPos = startPos;
+        this.endPos = endPos;
+        this.startTime = startTime;
+        this.duration = duration;
+    }
+
+    public Transform Target
+    {
+        get { return target; }
+        private set { target = value; }
+    }
+
+    public Vector3 StartPos
+    {
+        get { return startPos; }
+        private set { startPos = value; }
+    }
+
+    public Vector3 EndPos
+    {
+        get { return endPos; }
+        private set { endPos = value; }
+    }
+
+    public float StartTime
+    {
+        get { return startTime; }
+        private set { startTime = value; }
+    }
+
+    public float Duration
+    {
+        get { return duration; }
+        private set { duration = value; }
+    }
+}
