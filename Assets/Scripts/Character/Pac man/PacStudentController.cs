20dd3339c61cf3da7a120bb1fa1207bb69dfff57
Bryan Colin <bryancolin35@ymail.com>
2020-10-11 18:32:37 +0700

    Update on PacMan particle system

--- Assets/Scripts/Character/Pac man/PacStudentController.cs
+++ Assets/Scripts/Character/Pac man/PacStudentController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PacStudentController : MonoBehaviour
 {
     public Animator animator;
+    public ParticleSystem particleSystem;
 
     public AudioSource movementSource;
     public AudioClip[] movementClips;
@@ -14,11 +15,12 @@ public class PacStudentController : MonoBehaviour
     private Vector3 movement;
     private float movementSqrtMagnitude;
 
+    private float rotation;
+
     private Vector3 lastInput = Vector3.zero;
     private Vector3 currentInput = Vector3.zero;
 
-    private Vector3 dest = Vector3.zero;
-    private Vector3 dir = Vector3.zero;
+    private Vector3 destination = Vector3.zero;
 
     // Start is called before the first frame update
     void Start()
@@ -35,9 +37,10 @@ public class PacStudentController : MonoBehaviour
         //    CharacterPosition();
         //else
         //    movementSqrtMagnitude = 0.0f;
-
         CharacterPosition();
+        CharacterRotation();
         WalkingAnimation();
+        ParticlePlay();
         MovementAudio();
     }
 
@@ -53,7 +56,7 @@ public class PacStudentController : MonoBehaviour
             lastInput = Vector3.right;
         else if (movement.x < 0)
             lastInput = Vector3.left;
-        else if (movement.y > 0)        
+        else if (movement.y > 0)
             lastInput = Vector3.up;
         else if (movement.y < 0)
             lastInput = Vector3.down;
@@ -63,7 +66,7 @@ public class PacStudentController : MonoBehaviour
     {
         if (RayCastCheckPellet(lastInput))
         {
-            dest = lastInput + transform.position;
+            destination = lastInput + transform.position;
             currentInput = lastInput;
             Tweening(lastInput);
         }
@@ -71,7 +74,7 @@ public class PacStudentController : MonoBehaviour
         {
             if (RayCastCheckPellet(currentInput))
             {
-                dest = currentInput + transform.position;
+                destination = currentInput + transform.position;
                 Tweening(currentInput);
             }
         }
@@ -81,49 +84,79 @@ public class PacStudentController : MonoBehaviour
     bool RayCastCheckPellet(Vector3 inputDirection)
     {
         Vector3 pos = transform.position;
-        inputDirection += new Vector3(inputDirection.x * 0.45f, inputDirection.y * 0.45f);
+        inputDirection += new Vector3(inputDirection.x * 0.3f, inputDirection.y * 0.3f);
+
         RaycastHit2D hit = Physics2D.Linecast(pos + inputDirection, pos);
+        Debug.DrawLine(pos + inputDirection, pos, Color.yellow);
 
         if (hit)
         {
             if (hit.collider.CompareTag("NormalPellet") || hit.collider.CompareTag("PowerPellet") || (hit.collider == GetComponent<Collider2D>()))
+            {
                 return true;
+            }
+            if (hit.collider.CompareTag("Wall"))
+            {
+                return false;
+            }
         }
         return false;
     }
 
     void Tweening(Vector3 inputDirection)
     {
-        Vector3 destination = transform.position + inputDirection;
-        tweener.AddTween(transform, transform.position, destination, 0.3f);
+        tweener.AddTween(transform, transform.position, transform.position + inputDirection, 0.25f);
+    }
+
+    void CharacterRotation()
+    {
+        Vector3 direction = destination - transform.position;
+        movementSqrtMagnitude = direction.sqrMagnitude;
+
+        if (movementSqrtMagnitude != 0)
+        {
+            if (direction.x > 0)
+                transform.rotation = Quaternion.Euler(0.0f, 0.0f, 0.0f);
+            else if (direction.x < 0)
+                transform.rotation = Quaternion.Euler(0.0f, 180.0f, 0.0f);
+            else if (direction.y > 0)
+                transform.rotation = Quaternion.Euler(0.0f, 0.0f, 90.0f);
+            else if (direction.y < 0)
+                transform.rotation = Quaternion.Euler(0.0f, 0.0f, 270.0f);
+        }
     }
 
     void WalkingAnimation()
     {
-        Vector3 direction = dest - transform.position;
-        movementSqrtMagnitude = direction.sqrMagnitude;
+        Vector3 direction = destination - transform.position;
 
         animator.SetFloat("Speed", direction.sqrMagnitude);
-        animator.SetFloat("Horizontal", direction.x);
-        animator.SetFloat("Vertical", direction.y);
+        //animator.SetFloat("Horizontal", direction.x);
+        //animator.SetFloat("Vertical", direction.y);
+    }
+
+    void ParticlePlay()
+    {
+        Vector3 direction = destination - transform.position;        
+
+        if (direction.sqrMagnitude < 0.3f)
+        {
+            particleSystem.Stop();
+        }
+        else
+        {
+            particleSystem.Play();
+        }
     }
 
     void MovementAudio()
     {
         if (movementSqrtMagnitude >= 0.3f && !movementSource.isPlaying)
         {
-            if (isEating)
-            {
-                Debug.Log("Playing");
-                movementSource.clip = movementClips[0];
-                movementSource.Play();
-            }
-            else
-            {
-                Debug.Log("Playing other");
-                movementSource.clip = movementClips[1];
-                movementSource.Play();
-            }
+            Debug.Log("Playing movement");
+            movementSource.clip = movementClips[1];
+            movementSource.volume = 0.5f;
+            movementSource.Play();
         }
         else if (movementSqrtMagnitude <= 0.3f && movementSource.isPlaying)
         {
@@ -136,18 +169,24 @@ public class PacStudentController : MonoBehaviour
         Debug.Log("hit");
     }
 
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Wall")
+        {
+            Debug.Log(collision.gameObject.name);
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("NormalPellet") || collision.CompareTag("PowerPellet"))
+        if (collision.CompareTag("NormalPellet") || collision.CompareTag("PowerPellet") || collision.CompareTag("Cherry"))
         {
+            Debug.Log("Playing eating");
+            movementSource.volume = 0.1f;
+            movementSource.PlayOneShot(movementClips[0]);
+
             Destroy(collision.gameObject);
-            isEating = true;
             Debug.Log("eat pellet");
-        }
-        else
-        {
-            isEating = false;
-            Debug.Log("moving");
         }
     }
 }
