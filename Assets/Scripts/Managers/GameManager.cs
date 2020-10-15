d3033ce862aaf49940fd1e5eeae21da56301c978
Bryan Colin <bryancolin35@ymail.com>
2020-10-15 21:08:14 +0700

    Create GameManager.cs
    Update UIManager

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public enum GameState { StartScene, GameScene, DesignScene }
+
+    public static GameState currentGameState = GameState.StartScene;
+}
