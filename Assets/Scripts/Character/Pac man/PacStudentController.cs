cf2e89217022b0c4ae41b26576c9275f6f8749d8
Bryan Colin <bryancolin35@ymail.com>
2020-10-15 15:57:35 +0700

    Update PacStudentController.cs
    {use LevelGenerator to move in Grid}
    {Update on Audio}
    {Almost finish 70% overall}
    {80% in progress}

--- Assets/Scripts/Character/Pac man/PacStudentController.cs
+++ Assets/Scripts/Character/Pac man/PacStudentController.cs
@@ -7,6 +7,8 @@ public class PacStudentController : MonoBehaviour
     public Animator animator;
     public ParticleSystem particleSystem;
 
+    public LevelGenerator levelGenerator;
+
     public AudioSource movementSource;
     public AudioClip[] movementClips;
     private bool isEating = false;
@@ -15,12 +17,12 @@ public class PacStudentController : MonoBehaviour
     private Vector3 movement;
     private float movementSqrtMagnitude;
 
-    private float rotation;
-
     private Vector3 lastInput = Vector3.zero;
     private Vector3 currentInput = Vector3.zero;
 
     private Vector3 destination = Vector3.zero;
+
+    private int xPosition, yPosition;
 
     // Start is called before the first frame update
     void Start()
@@ -37,6 +39,7 @@ public class PacStudentController : MonoBehaviour
         //    CharacterPosition();
         //else
         //    movementSqrtMagnitude = 0.0f;
+
         CharacterPosition();
         CharacterRotation();
         WalkingAnimation();
@@ -80,23 +83,117 @@ public class PacStudentController : MonoBehaviour
         }
     }
 
-    // Using RayCast to check if Grid is Walkable
+    // Using LevelMapGenerator to check if Grid is Walkable
     bool RayCastCheckPellet(Vector3 inputDirection)
     {
+        // Top Left
+        if (transform.position.x + inputDirection.x <= 0 && transform.position.y + inputDirection.y >= 0) 
+        {
+            xPosition = (int)((transform.position.x + 13.5f) + inputDirection.x);
+            yPosition = (int)(Mathf.Abs(transform.position.y - 14) + -inputDirection.y);
+
+            //Debug.Log("X : " + xPosition + " Y : " + yPosition);
+
+            if (levelGenerator.levelMap[yPosition, xPosition] == 0)
+            {
+                movementSource.clip = movementClips[1];
+                return true;
+            }
+            else if (levelGenerator.levelMap[yPosition, xPosition] == 5 || levelGenerator.levelMap[yPosition, xPosition] == 6)
+            {
+                movementSource.clip = movementClips[0];
+                levelGenerator.levelMap[yPosition, xPosition] = 0;
+                return true;
+            }
+        }
+
+        // Top Right
+        if (transform.position.x + inputDirection.x >= 0 && transform.position.y + inputDirection.y >= 0)
+        {
+            xPosition = (int)((transform.position.x - 0.5f) + inputDirection.x);
+            yPosition = (int)(Mathf.Abs(transform.position.y - 14) + -inputDirection.y);
+
+            //Debug.Log("X : " + xPosition + " Y : " + yPosition);
+
+            if (levelGenerator.levelMapTopRight[yPosition, xPosition] == 0)
+            {
+                movementSource.clip = movementClips[1];
+                return true;
+            }
+            else if (levelGenerator.levelMapTopRight[yPosition, xPosition] == 5 || levelGenerator.levelMapTopRight[yPosition, xPosition] == 6)
+            {
+                movementSource.clip = movementClips[0];
+                levelGenerator.levelMapTopRight[yPosition, xPosition] = 0;
+                return true;
+            }
+        }
+
+        // Bottom Left
+        if (transform.position.x + inputDirection.x < 0 && transform.position.y + inputDirection.y < 0)
+        {
+            xPosition = (int)((transform.position.x + 13.5f) + inputDirection.x);
+            yPosition = (int)Mathf.Abs((transform.position.y + 1) + inputDirection.y);
+
+            //Debug.Log("X : " + xPosition + " Y : " + yPosition);
+            //Debug.Log(levelGenerator.levelMapBottomLeft[yPosition, xPosition]);
+
+            if (levelGenerator.levelMapBottomLeft[yPosition, xPosition] == 0)
+            {
+                movementSource.clip = movementClips[1];
+                return true;
+            }
+            else if (levelGenerator.levelMapBottomLeft[yPosition, xPosition] == 5 || levelGenerator.levelMapBottomLeft[yPosition, xPosition] == 6)
+            {
+                movementSource.clip = movementClips[0];
+                levelGenerator.levelMapBottomLeft[yPosition, xPosition] = 0;
+                return true;
+            }
+        }
+
+        // Bottom Right
+        if (transform.position.x + inputDirection.x > 0 && transform.position.y + inputDirection.y < 0)
+        {
+            xPosition = (int)((transform.position.x - 0.5f) + inputDirection.x);
+            yPosition = (int)Mathf.Abs((transform.position.y + 1) + inputDirection.y);
+
+            //Debug.Log("X : " + xPosition + " Y : " + yPosition);
+            //Debug.Log(levelGenerator.levelMapBottomRight[yPosition, xPosition]);
+
+            if (levelGenerator.levelMapBottomRight[yPosition, xPosition] == 0)
+            {
+                movementSource.clip = movementClips[1];
+                return true;
+            }
+            else if (levelGenerator.levelMapBottomRight[yPosition, xPosition] == 5 || levelGenerator.levelMapBottomRight[yPosition, xPosition] == 6)
+            {
+                movementSource.clip = movementClips[0];
+                levelGenerator.levelMapBottomRight[yPosition, xPosition] = 0;
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    // Using RayCast to check if Grid is Walkable
+    bool RayCastCheckWall(Vector3 inputDirection)
+    {
         Vector3 pos = transform.position;
-        inputDirection += new Vector3(inputDirection.x * 0.3f, inputDirection.y * 0.3f);
+        inputDirection += new Vector3(inputDirection.x * 0.45f, inputDirection.y * 0.45f);
 
         RaycastHit2D hit = Physics2D.Linecast(pos + inputDirection, pos);
         Debug.DrawLine(pos + inputDirection, pos, Color.yellow);
 
         if (hit)
         {
-            if (hit.collider.CompareTag("NormalPellet") || hit.collider.CompareTag("PowerPellet") || (hit.collider == GetComponent<Collider2D>()))
+            if (hit.collider.CompareTag("NormalPellet") || hit.collider.CompareTag("PowerPellet") || hit.collider.CompareTag("Portal") || (hit.collider == GetComponent<Collider2D>()))
             {
+                Debug.Log("Normal Pellet");
                 return true;
             }
             if (hit.collider.CompareTag("Wall"))
             {
+                Debug.Log("Wall");
                 return false;
             }
         }
@@ -105,7 +202,7 @@ public class PacStudentController : MonoBehaviour
 
     void Tweening(Vector3 inputDirection)
     {
-        tweener.AddTween(transform, transform.position, transform.position + inputDirection, 0.25f);
+        tweener.AddTween(transform, transform.position, transform.position + inputDirection, 0.3f);
     }
 
     void CharacterRotation()
@@ -130,14 +227,21 @@ public class PacStudentController : MonoBehaviour
     {
         Vector3 direction = destination - transform.position;
 
-        animator.SetFloat("Speed", direction.sqrMagnitude);
+        if (direction.sqrMagnitude < 1.0f)
+        {
+            animator.SetFloat("Speed", 0);
+        }
+        else
+        {
+            animator.SetFloat("Speed", direction.sqrMagnitude);
+        }
         //animator.SetFloat("Horizontal", direction.x);
         //animator.SetFloat("Vertical", direction.y);
     }
 
     void ParticlePlay()
     {
-        Vector3 direction = destination - transform.position;        
+        Vector3 direction = destination - transform.position;
 
         if (direction.sqrMagnitude <= 0.3f)
         {
@@ -151,16 +255,22 @@ public class PacStudentController : MonoBehaviour
 
     void MovementAudio()
     {
-        if (movementSqrtMagnitude >= 0.3f && !movementSource.isPlaying)
+        Debug.Log(movementSqrtMagnitude);
+        if (movementSqrtMagnitude == 1.0f)
         {
-            Debug.Log("Playing movement");
-            movementSource.clip = movementClips[1];
-            movementSource.volume = 0.5f;
-            movementSource.Play();
+            if (!movementSource.isPlaying)
+            {
+                Debug.Log("Playing movement");
+                movementSource.volume = 0.5f;
+                movementSource.Play();
+            }
         }
-        else if (movementSqrtMagnitude <= 0.3f && movementSource.isPlaying)
+        else if (movementSqrtMagnitude < 1.0f)
         {
-            movementSource.Stop();
+            if (movementSource.isPlaying)
+            {
+                movementSource.Stop();
+            }
         }
     }
 
@@ -181,13 +291,29 @@ public class PacStudentController : MonoBehaviour
     {
         if (collision.CompareTag("NormalPellet") || collision.CompareTag("PowerPellet") || collision.CompareTag("Cherry"))
         {
-            Debug.Log("Playing eating");
-            movementSource.clip = movementClips[0];
-            movementSource.volume = 0.1f;
-            movementSource.PlayOneShot(movementClips[0]);
-
+            //if (movementSource.isPlaying)
+            //{
+            //    Debug.Log("Playing eating");
+            //    //movementSource.clip = movementClips[0];
+            //    movementSource.volume = 0.1f;
+            //    movementSource.Play();
+            //}
             Destroy(collision.gameObject);
             Debug.Log("eat pellet");
         }
+
+        //if (collision.CompareTag("Portal"))
+        //{
+        //    if (this.transform.position.x < 0)
+        //    {
+        //        Debug.Log("Left");
+        //        this.transform.position = new Vector3(13.0f, 0.0f, 0.0f);
+        //    }
+        //    else if (this.transform.position.x > 0)
+        //    {
+        //        Debug.Log("Right");
+        //        this.transform.position = new Vector3(-13.0f, 0.0f, 0.0f);
+        //    }
+        //}
     }
 }
