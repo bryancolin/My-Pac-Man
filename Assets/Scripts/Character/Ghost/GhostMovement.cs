e62ae317209c89094c5e9e32d2df4c960c151554
Bryan Colin <bryancolin35@ymail.com>
2020-10-16 16:29:20 +0700

    Create GhostScript

---
+++ Assets/Scripts/Character/Ghost/GhostMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostMovement : MonoBehaviour
+{
+    private AudioManager backgroundMusic;
+
+    [SerializeField]
+    private GameObject[] ghost;
+
+    private void Awake()
+    {
+        for (int i = 0; i < ghost.Length; i++)
+        {
+            ghost[i].GetComponent<Animator>().SetFloat("Speed", 1);
+        }
+
+        backgroundMusic = GameObject.FindWithTag("Managers").GetComponent<AudioManager>();
+        backgroundMusic.ChangeBackgroundMusic(1);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void SetScared()
+    {
+        for(int i=0; i<ghost.Length; i++)
+        {
+            ghost[i].GetComponent<Animator>().SetTrigger("Scared");
+        }
+        backgroundMusic.ChangeBackgroundMusic(2);
+    }
+}
