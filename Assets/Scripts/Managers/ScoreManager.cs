b322b787033a6cda582028c38e5cee579ece30d9
Bryan Colin <bryancolin35@ymail.com>
2020-10-21 21:18:08 +0700

    Create getter and setter on ScoreManager

--- Assets/Scripts/Managers/ScoreManager.cs
+++ Assets/Scripts/Managers/ScoreManager.cs
@@ -4,16 +4,27 @@ using UnityEngine;
 
 public class ScoreManager : MonoBehaviour
 {
-    public int score;
+    //public int score;
+    private int totalPellets;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void SetTotalPellet(int pellet)
+    {
+        totalPellets += pellet;
+    }
+
+    public int GetTotalPellets()
+    {
+        return totalPellets;
     }
 }
